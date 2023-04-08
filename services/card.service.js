471a0ba4ccb84ed39552ad85c4ea3d17c54d57cc
Nem791 <phuongnam131201.nnnpn@gmail.com>
2023-04-08 14:29:30 +0700

    update

--- services/card.service.js
+++ services/card.service.js
@@ -9,13 +9,13 @@ const {
 } = require("firebase/storage");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzUSVGJnNVhJGUqXBVg1i73jODq92Z0TA",
-  authDomain: "flashcard-uploads.firebaseapp.com",
-  projectId: "flashcard-uploads",
-  storageBucket: "flashcard-uploads.appspot.com",
-  messagingSenderId: "58873052864",
-  appId: "1:58873052864:web:f10a52d712e2f72438f725",
-  measurementId: "G-HG5SXFN18Y",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
